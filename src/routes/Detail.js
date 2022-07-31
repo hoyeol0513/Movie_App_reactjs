1baab351c9af15ed6d20c023b0697a210737fdc8
hoyeol0513 <hoyeol0513@naver.com>
2022-07-31 14:30:46 +0900

    Detail페이지 params 추가하여 id링크 만들기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+//route에서 "/movie/:id"를 사용하여 id를 인자로 받아오려면 useParams 필요
 function Detail(){
+  const { id } = useParams();
+  const getMovie = async ()  => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect (()=>{
+    getMovie();
+  },[]);
   return <h1>Detail</h1>;
 }
 
